df2ecbb10e9843efa73d4a0abc5f1d037ce48dac
Hasibul Islam <hasibulislam1670@gmail.com>
2024-02-23 19:45:59 +0600

    learn :: TimeComplexity

---
+++ TimeComplexity.java
@@ -0,0 +1,22 @@
+public class TimeComplexity {
+  public static void main(String[] args) {
+    double now = System.currentTimeMillis();
+    TimeComplexity demo = new TimeComplexity();
+    System.out.println(demo.findSum(99999));
+    System.out.println("Time Taken - " + (System.currentTimeMillis() - now) + "millises.");
+  }
+
+  public int findSum(int n){
+  return n*(n+1)/2;
+  }
+
+  // public int findSum(int n) {
+  //   int sum = 0;
+  //   for (int i = 1; i <= n; i++) {
+  //     sum = sum + i;
+  //   }
+  //   return sum;
+  // }
+
+
+}
